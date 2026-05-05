0d896c54f6c1b3864e8811e9909509c3556227f2
kiraTheresa <2312190107@pop.zjgsu.edu.cn>
2026-05-05 13:51:59 +0800

    07 - CICD

--- miniprogram/mp-user/tests/setup.ts
+++ miniprogram/mp-user/tests/setup.ts
@@ -1,5 +1,7 @@
 import { WxMock } from './mocks/wx-mock';
 
+const isCI = process.env.CI === 'true' || process.env.CI === '1';
+
 declare global {
   namespace NodeJS {
     interface Global {
@@ -12,11 +14,19 @@ declare global {
 
 beforeEach(() => {
   (global as any).wx.clearAllMocks();
+  if (isCI) {
+    jest.setTimeout(15000);
+  }
 });
 
 afterEach(() => {
   jest.clearAllMocks();
 });
+
+if (isCI) {
+  jest.setTimeout(15000);
+  console.log('Running in CI environment with extended timeouts');
+}
 
 expect.extend({
   toBeSuccessfulResponse(received: any) {
